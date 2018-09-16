a24f1eb834f21aed48796c184401007f3459f416
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-09-16 00:52:01 -0400

    Starting to change css to a pre complier of scss also added more accessbility and fading animations

--- src/Components/Intro/index.jsx
+++ src/Components/Intro/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import './index.css';
+import './CSS/main.css';
 
 
 const Intro = props =>  (
-<div className='pageOne'>
-<div className="block">
-<h1 className='j1'>Cory Hrycko</h1>
-  <h3 className='j3'> &lt;Something Sick&gt; </h3>
-  </div>
-<div className ="btnlistTextCenter">
-  <a className ="btn btn-default" href="*"><i class="fa fa-twitter fa-lg" id="twitterIcon" aria-hidden="true"></i>
+  <div className='pageOne'>
+    <div className='fadeBlock1'>
+    <div className='fadeBlock2'>
+      <div className="block">
+          <div className='anim'></div>
+          <h1 className='j1'>Cory Hrycko</h1>
+          </div>
+            <h3 className='j3'> &lt;Something Sick&gt; </h3>
+        <div className ="btnlistTextCenter">
+          <a className ="btn btn-default" href="*"><i class="fa fa-twitter fa-lg" id="twitterIcon" aria-hidden="true"></i>
 Twitter</a>
-  <a className ="btn btn-default" href="*"><i class="fa fa-github fa-lg" aria-hidden="true" id="githubIcon"></i> Github</a>
-  <a className ="btn btn-default" href="*"><i class="fa fa-linkedin fa-lg" aria-hidden="true" id="linkedinIcon"></i> LinkedIn</a>
-<a className ="btn btn-default" href="*"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"id="facebookIcon"></i> FaceBook</a>
-</div>
-</div>
-)
+          <a className ="btn btn-default" href="*"><i class="fa fa-github fa-lg" aria-hidden="true" id="githubIcon"></i> Github</a>
+          <a className ="btn btn-default" href="*"><i class="fa fa-linkedin fa-lg" aria-hidden="true" id="linkedinIcon"></i> LinkedIn</a>
+          <a className ="btn btn-default" href="*"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"id="facebookIcon"></i> FaceBook</a>
+          </div>
+        </div>
+      </div>
+      </div>
+);
 export default Intro;
